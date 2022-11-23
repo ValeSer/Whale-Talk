2019e95556d15bdf0552389bcfd228d9f2f9ecab
Valeria Serci <balerty84@gmail.com>
2022-11-23 17:01:37 +0000

    get a final string and test it

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-const input = 'Welcome';
+const input = 'turpentine and turtles';
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 const resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-    if (input[i] === 'e') {
-      resultArray.push(input[i]);
-    } else if (input[i] === 'u') {
-      resultArray.push(input[i]);}
-    for (let j = 0; j <vowels.length; j++) {
-      if (input[i] === vowels[j]) {
-        resultArray.push(vowels[j]);
-      }  
-    }
+  if (input[i] === 'e') {
+    resultArray.push(input[i]);
+  } else if (input[i] === 'u') {
+    resultArray.push(input[i]);}
+  for (let j = 0; j <vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(vowels[j]);
+    }  
   }
-  
-  console.log(resultArray);
+}
+
+resultString = resultArray.join('').toUpperCase();
+console.log(resultString);
