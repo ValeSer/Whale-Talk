0f92f4d421b849315ce66d1f05352fcdc6d18367
Valeria Serci <balerty84@gmail.com>
2022-11-23 16:49:15 +0000

    test the function

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ for (let i = 0; i < input.length; i++) {
     for (let j = 0; j <vowels.length; j++) {
       if (input[i] === vowels[j]) {
         resultArray.push(vowels[j]);
+        console.log(resultArray);
       }  
     }
   }
