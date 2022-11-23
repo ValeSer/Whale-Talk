134843b785681d8d27cd6b22c3ccbb560f5c3774
Valeria Serci <balerty84@gmail.com>
2022-11-23 16:57:10 +0000

    add if statement that double e and u if found

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ const vowels = ['a', 'e', 'i', 'o', 'u'];
 const resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
+    if (input[i] === 'e') {
+      resultArray.push(input[i]);
+    } else if (input[i] === 'u') {
+      resultArray.push(input[i]);}
     for (let j = 0; j <vowels.length; j++) {
       if (input[i] === vowels[j]) {
         resultArray.push(vowels[j]);
-        console.log(resultArray);
       }  
     }
   }
+  
+  console.log(resultArray);
